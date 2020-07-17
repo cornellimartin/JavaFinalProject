7d103ca17e5b25380572400ea523320f4219832b
natlat13 <lat10002@byui.edu>
2020-07-17 16:57:00 -0600

    ambas

--- src/FinalProject/Ambassadors.java
+++ src/FinalProject/Ambassadors.java
@@ -1,4 +1,14 @@
 package FinalProject;
 
 public class Ambassadors extends GirlScout {
+    public Ambassadors() {
+    }
+    public Ambassadors(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String getReward() {
+        return "Disneyland";
+    }
 }
